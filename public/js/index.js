9413ccc657099c18c6749aba9cdd5da10f83e56c
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2018-03-08 14:18:34 +0545

    emit to multiple users

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ let socket = io()
 socket.on('connect', ()=>{
     console.log("Connected to the server");
 
-    socket.emit('createMessage', {
-        from: "uzz@gmail.com",
-        text: "I am doing jst fine man!"
-    })
-
 })
 
 socket.on('newMessage', (message)=>{
--- server/server.js
+++ server/server.js
@@ -15,16 +15,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log("New User connected!");
 
-    socket.emit('newMessage', {
-        from: "johndoe@example.com",
-        text: "how you doin these days",
-        createdAt: 12345
+    socket.on('createMessage', (message)=>{
+        console.log("Created Message", message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
     })
-
-    socket.on('createMessage', (newMessage)=>{
-        console.log("Created Message", newMessage);
-    })
-
     socket.on('disconnect', () => {
         console.log("User disconnected")
     })
